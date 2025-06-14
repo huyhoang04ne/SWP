3d55596e8696495b83941e3cf478ceea1bd9d1fc
huyhoang04ne <phanngochuyhoang1710@gmail.com>
2025-06-14 13:21:23 +0700

    push project

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SWP391.Models
+{
+    public class User
+    {
+        public required string Id { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string FullName { get; set; }
+        public required string Gender { get; set; }
+        public required string Phone { get; set; }
+        public required string Address { get; set; }
+        public required string RoleId { get; set; }
+        public required string? MedicalHistory { get; set; }
+        public required string PrivacySettings { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+        public bool TwoFactorEnabled { get; set; }
+
+        public bool IsActive { get; set; }
+        public bool IsVerified { get; set; } = true;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SWP391.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm dịch vụ MVC (Controllers + Views)
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor();
+
+
+// Kích hoạt Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // thời gian session sống
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Cấu hình DbContext kết nối SQL Server
+builder.Services.AddDbContext<GhmsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GHMSConnection")));
+
+var app = builder.Build();
+
+// Kích hoạt các middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Kích hoạt Session
+app.UseSession();
+
+app.UseAuthorization();
+
+// Cấu hình route mặc định
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
